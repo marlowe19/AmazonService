8ade68d1a8e8a165dca45621ae18bd1db3069fdc
Marlowe <Marlowe@MARLOWETJE>
2017-03-30 10:16:10 +0200

    Initial commit

---
+++ src/Web Apps/AWS.WEB/AWS.WEB/Abstractions/IAmazonService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWS.MVC.Abstractions
+{
+    interface IAmazonService
+    {
+        List<string> GetAllInstances();
+        void Login();
+
+    }
+}
---
+++ src/Web Apps/AWS.WEB/AWS.WEB/Services/AmazonService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AWS.MVC.Abstractions;
+
+namespace AWS.MVC.Services
+{
+    public class AmazonService: IAmazonService
+    {
+        public List<string> GetAllInstances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
