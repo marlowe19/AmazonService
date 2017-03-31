4a200f0c9c4a97f4556eb85d7e443446ba2393f6
Marlowe Antonius <marlowe.antonius@incentro.com>
2017-03-31 11:20:51 +0200

    Update switch.ps1

--- switch.ps1
+++ switch.ps1
@@ -1,2 +1,12 @@
-Test-Path "C:\Program Files\Docker\dockercli.exe"
- & 'C:\Program Files\Docker\DockerCli.exe' -SwitchDaemon
+$items = Get-ChildItem -Path "C:\Program Files\Docker"
+ 
+# enumerate the items array
+foreach ($item in $items)
+{
+      # if the item is a directory, then process it.
+     
+            Write-Host $item.Name
+    
+}
+#Test-Path "C:\Program Files\Docker\dockercli.exe"
+ #& 'C:\Program Files\Docker\DockerCli.exe' -SwitchDaemon
